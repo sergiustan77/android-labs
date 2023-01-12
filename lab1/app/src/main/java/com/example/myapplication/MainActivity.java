169980df7d36c6cdd564b8dc6092a4c160446ca8
Sergiu Stan <sergiustan007@gmail.com>
2023-01-12 12:53:53 +0200

    added Lab1

---
+++ lab1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendInfo(View view) {
+        TextView welcomeText = findViewById(R.id.welcomeText);
+        TextView textFirstName = findViewById(R.id.textFirstName);
+        TextView textLastName = findViewById(R.id.textLastName);
+        TextView textEmail = findViewById(R.id.textEmail);
+
+        EditText inputFirstName = findViewById(R.id.inputFirstName);
+        EditText inputLastName = findViewById(R.id.inputLastName);
+        EditText inputEmail = findViewById(R.id.inputEmail);
+
+
+        welcomeText.setText("Hello " + inputFirstName.getText().toString() + "!");
+        textFirstName.setText("First Name: " + inputFirstName.getText().toString());
+        textLastName.setText("Last Name: " + inputLastName.getText().toString());
+        textEmail.setText("Email: " + inputEmail.getText().toString());
+    }
+}
